f9c1e975107efba86e6fc87228629f7873c2f0f5
mitchya1 <mitchell.anderson@colorado.edu>
2019-02-20 14:21:15 -0700

    mongo load is working

--- app/abuse_scraper.py
+++ app/abuse_scraper.py
@@ -1,12 +1,60 @@
+import requests, csv, json, mongo_handler
 from config import *
 from app_logger import *
-import requests, csv
+from sys import exit
+from time import sleep
 
 abuse_csv_response = requests.get(RANSOMWARE_TRACKER_URL)
 
 if abuse_csv_response.status_code != 200:
     logger.error("Ransomware call returned a status code of {}".format(str(abuse_csv_response.status_code)))
+    exit(1)
 else:
-    logger.info("Successfully called Ransomware CSV")
+    logger.info("Successfully retrieved Ransomware CSV")
 
 
+# Firstseen (UTC),Threat,Malware,Host,URL,Status,Registrar,IP address(es),ASN(s),Country
+#ransom_csv = csv.reader(abuse_csv_response.text.read().decode('ascii')) 
+ransom_csv = csv.reader(abuse_csv_response.text.splitlines(), delimiter=',')
+try:
+    ransom_list = list(ransom_csv)
+except Exception as e:
+    logger.error("Unable to listify CSV :: {}".format(str(e)))
+
+json_record = {}
+
+for row in ransom_list:
+    if len(row) < 2:
+        pass
+    else:
+        firstSeen = row[0]
+        threat = row[1]
+        if threat == "Threat":
+            # Skip the rows that tell us the CSV structure
+            pass
+        else:
+            malware = row[2]
+            host = row[3]
+            url = row[4]
+            status = row[5]
+            registrar = row[6]
+            IP = row[7]
+            # Mongo won't let you have a key with dots in it
+            keyHost = str(host).replace(".", "_")
+            ASN = row[8]
+            country = row[9]
+            if IP == "":
+                IP = "null"
+            if url == "":
+                url = "null"
+            json_record = { keyHost: {"Threat": threat, "Malware": malware, "Host": host, "URL": url, "Status": status, \
+                "Registrar": registrar, "IP": IP, "ASN": ASN, "Country": country, "FirstSeen": firstSeen}}
+            print(json.dumps(json_record))
+            mongo_handler.loader(json_record)
+            sleep(1)
+            """
+            if mongo_handler.loader(json_record):
+                logger.info("Successful response from Mongo Handler")
+            else:
+                logger.error("Failure response from Mongo Handler")
+            """
--- app/mongo_handler.py
+++ app/mongo_handler.py
@@ -1,10 +1,18 @@
 from pymongo import MongoClient
 from config import *
+from app_logger import *
 
 client = MongoClient(MONGO_URL)
-db = client(MONGO_DB)
+db = client[MONGO_DB]
 
 def loader(json_record):
-    print("hello")
+    try:
+        db[MONGO_COLLECTION].insert_one(json_record)
+        logger.info("Inserted record into Mongo")
+        return True
+    except Exception as e:
+        logger.error(str(e))
+        return False
+
 
 
