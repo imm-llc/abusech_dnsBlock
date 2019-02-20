b08966d3102362446379b6e6ef9e0c464dc53545
mitchya1 <mitchell.anderson@colorado.edu>
2019-02-20 12:35:17 -0700

    add example config

---
+++ app/config.example.py
@@ -0,0 +1,20 @@
+MONGO_URL = 'mongodb://localhost:27017'
+"""
+For authenticated mongo, use:
+MONGO_URL = 'mongodb://userName:passWord@localhost:27017'
+For remote authenticated mongo, use:
+MONGO_URL = 'mongodb://userName:passWord@remoteHost:27017'
+
+Change the port if needed
+"""
+
+RANSOMWARE_TRACKER_URL = "https://ransomwaretracker.abuse.ch/feeds/csv/"
+
+# Available options are:
+# dev
+# prod
+# none
+# dev will print errors
+# prod will log to file
+LOG_MODE = "dev"
+LOG_FILE = "/path/to/logfile"
