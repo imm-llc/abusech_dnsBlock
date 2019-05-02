9c66948e135132d90e2bbf292a608a79adc5fe29
mitchya1 <mitchell.anderson@colorado.edu>
2019-05-02 14:31:29 -0600

    optimize imports, change func name

--- app/mongo_handler.py
+++ app/mongo_handler.py
@@ -1,20 +1,20 @@
 from pymongo import MongoClient
-from config import *
-from app_logger import *
+from config import MONGO_URL, MONGO_DB, MONGO_COLLECTION
+from app_logger import logger
 import json
 from bson import json_util
 
 client = MongoClient(MONGO_URL)
 db = client[MONGO_DB]
 
-def loader(json_record):
+def insert_malicious_record(json_record):
     try:
         json_record = json_util.loads(json_record)
         db[MONGO_COLLECTION].insert_one(json_record)
         logger.info("Inserted record into Mongo")
         return True
     except Exception as e:
-        logger.error(str(e))
+        logger.error("Error inserting record into mongo: {}".format(str(e)))
         return False
 
 
